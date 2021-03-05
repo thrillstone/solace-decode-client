e7a5a911cbadfee21364a256e3bb76e9d55fa5ea
farazsfh <farazhussain2000@gmail.com>
2021-03-04 20:36:49 -0500

    adding message broker for messages

--- src/Messages.js
+++ src/Messages.js
@@ -4,32 +4,35 @@ import Message from './Message'
 
 function Messages(props) {
 	const messaging = useContext(EventContext);
-    const [exampleReceived, setExampleReceived] = useState(false);
+    const [connected, setConnected] = useState(false);
+	
     const [messages, setMessages] = useState([{type: "message", id: 0, userId: 0, name: "Faraz", text: "Hello, world!", timestamp: "1:46 PM"}, {type: "message", id: 1, userId: 1, name: "World", text: "Hi, Faraz!", timestamp: "1:47 PM"}]);
 	const [text, setText] = useState("")
 
 	useEffect(() => {
-		// const fetchMessages = () => {
-		// 	fetch(`http://localhost:8080/channels/${channel.id}/messages`)
-		// 	.then((response) => {
-		// 		setMessages(response);
-		// 	})
-		// 	.catch((error) => {
-		// 		console.error(error);
-		// 	});
-		// };
-		// fetchMessages();
-	}, [messages, props.channel]);
-	
-	function sendMessage() {
-		console.log(text)
+		const setupMessaging = () => {
+			messaging.connect()
+			.then(() => {
+				setConnected(true);
+			})
+			.catch(console.error);
+			messaging.on("message", (event) => {
+				messages.push(event);
+				setMessages(messages);
+			});
+			messaging.subscribe(`channels/*/messages`);
+		};
+		setupMessaging();
+	}, [messaging]);
+
+	const publishMessage = () => {
+		if (connected){
+			messaging.publish(`channels/${props.selectedChannel.id}/messages`, {type: "message", id: 0, userId: 0, name: "Faraz", text: text, timestamp: "1:46 PM"});
+		}
 	}
 	
 	return (
 		<div className="Messages">
-            <div className="chat_header">
-				<h1>Header</h1>
-            </div>
             <div className="chat_body">
 				{
 					messages.map((message) =>
@@ -38,8 +41,12 @@ function Messages(props) {
 				}
             </div>
 			<div className="chat_footer">
-				<input value={text} onChange={(e) => setText(e.target.value)} placeholder="type something" />
-				<button type="submit" disabled={!text} onClick={sendMessage}>↩️</button>
+				<div>
+					<input className="chat_textarea" value={text} onChange={(e) => setText(e.target.value)} placeholder="Send a message" />
+				</div>
+				<div>
+					<button type="submit" disabled={!text} onClick={publishMessage}>↩️</button>
+				</div>
             </div>
 		</div>
 	);
