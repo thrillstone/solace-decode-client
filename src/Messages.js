797bb22aef1df6da8091039c526107788721cfbb
Jeffrey Douangpaseuth <jdouangpaseuth@hotmail.ca>
2021-03-05 15:15:09 -0500

    add channel summary

--- src/Messages.js
+++ src/Messages.js
@@ -16,6 +16,7 @@ function Messages(props) {
 	
 	const [messages, setMessages] = useState([{type: "message", channelId: 0, id: 0, userId: 0, name: "Faraz", text: "Hello, world! This is a really long message. This is a really long message. This is a really long message. This is a really long message. This is a really long message. This is a really long message.", timestamp: "1:46"}, {type: "message", channelId: 0, id: 1, userId: 1, name: "World", text: "Hi, Faraz! https://www.reddit.com/r/all/", timestamp: "1:47"}]);
 	const [text, setText] = useState("")
+	const [summaryVisibile, setSummaryVisibile] = useState(false);
 
 	useEffect(() => {
 		const setupMessaging = () => {
@@ -37,6 +38,10 @@ function Messages(props) {
 		}
 	}, [props.channel]);
 
+	const toggleSummaryVisible = () => {
+		setSummaryVisibile(!summaryVisibile);
+	}
+
 	const publishMessage = () => {
 		if (props.channel) {
 			console.log("Publishing to channel", props.channel)
@@ -46,6 +51,31 @@ function Messages(props) {
 
 	return (
 		<div className="Messages">
+			<div className="channel_summary">
+				<div className="summary_header" onClick={toggleSummaryVisible}>
+					<div className="summary_title">{props.channel.name}</div>
+					{summaryVisibile &&
+						<div className="read_status">
+							<div><b>80</b> Unread messages</div>
+							<div><b>0</b> Mentions</div>
+						</div>
+					}
+				</div>
+				{summaryVisibile &&
+					<div className="summary_content">
+						<div className="summary_detail">
+							<div className="summary_subtitle">Main Topics</div>
+							<div className="topic">
+								<div className="topic_name">"cats"</div>
+								<div className="topic_confidence"><b>90%</b> confidence</div>
+							</div>
+						</div>
+						<div className="summary_section">
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. 
+						</div>
+					</div>
+				}
+			</div>
 			<div className="chat_body">
 				{
 					messages.map((message) =>
